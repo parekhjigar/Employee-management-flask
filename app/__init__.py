30e33cf82b57b3b6d29aad0339891bf53c38e165
parekhjigar <hello@parekhjigar.com>
2020-01-23 07:30:47 +1100

    Initialized LoginManager object

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import app_config
 
-# db variable initialization
+# variable initialization
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config = True)
@@ -14,9 +15,8 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    # Testing route
-    @app.route('/')
-    def hello_world():
-        return 'Testing! It works.'
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
 
     return app
