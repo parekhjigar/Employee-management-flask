804606c77e7cd9c335327a6b47d7a643a4eb749e
parekhjigar <hello@parekhjigar.com>
2020-01-24 14:49:43 +1100

    Added support for flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+
 
 from config import app_config
 
@@ -14,6 +16,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config = True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    
+    Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
@@ -21,6 +25,8 @@ def create_app(config_name):
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
+
+
     from app import models
 
     from .admin import admin as admin_blueprint
