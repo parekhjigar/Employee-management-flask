e713a06103defffab6bf8607e60e9d95375f04b7
parekhjigar <hello@parekhjigar.com>
2020-01-24 17:55:45 +1100

    Added redirect link to admin dashboard if login as admin

--- app/auth/views.py
+++ app/auth/views.py
@@ -50,8 +50,11 @@ def login():
             # log employee in
             login_user(employee)
 
-            # redirect to the dashboard page after login
-            return redirect(url_for('home.dashboard'))
+            # redirect to the appropriate dashboard page
+            if employee.is_admin:
+                return redirect(url_for('home.admin_dashboard'))
+            else:
+                return redirect(url_for('home.dashboard'))
 
         # when login details are incorrect
         else:
