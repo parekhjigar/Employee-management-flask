88196f254cac1912495285f7518ce8445e4c5d17
parekhjigar <hello@parekhjigar.com>
2020-01-25 12:17:55 +1100

    Added department form

---
+++ app/admin/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class DepartmentForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
